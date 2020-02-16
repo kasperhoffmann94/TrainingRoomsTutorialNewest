459078d701f55a8aed732622e7ce02e15cd8eb8c
Kasper <kasperhoffmann@hotmail.dk>
2020-02-16 12:02:16 +0100

    add more styling

--- TrainingRooms.UWP/MainPage.xaml.cs
+++ TrainingRooms.UWP/MainPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using Windows.UI.Xaml.Controls;
+using TrainingRooms;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +13,11 @@ namespace TrainingRooms.UWP
         public MainPage()
         {
             this.InitializeComponent();
+            var repo = new RoomRepository();
+            var rooms = repo.GetRooms();
+
+            this.DataContext = rooms;
+
         }
     }
 }
